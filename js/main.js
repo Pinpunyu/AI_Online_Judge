03ae89779369539ca0fd0378bbd75cd29e334d0c
Pinpunyu <pe25617259@gmail.com>
2023-01-04 02:46:25 +0800

    debug

--- js/main.js
+++ js/main.js
@@ -88,13 +88,14 @@ function selectTextFile(files) {
 function saveDynamicDataToFile() {
 
 	var code = editor.getValue();
-	//const formData = new FormData();
+	var formData = new FormData();
 	const blob = new Blob([code], { type: "text/plain;charset=utf-8" });
-	//formData.append('file', blob, 'readme.txt');
+	formData.append('file', blob, 'readme.txt');
+	//xhr.send(formData);
 
 	const request = new XMLHttpRequest();
-	request.open('POST', './fileupload');
-	request.send(blob);
+	request.open('POST', 'http://140.127.208.185/OnlineJudge/fileupload');
+	request.send(formData);
 	// var blob = new Blob([code], {
 	// 	type: "text/plain;charset=utf-8",
 	// });
