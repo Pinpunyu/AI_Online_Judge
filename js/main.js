d17c8eceb0a12d4bb9d5760d083822a1e82fd6c6
Pinpunyu <pe25617259@gmail.com>
2023-01-04 01:17:12 +0800

    debug

--- js/main.js
+++ js/main.js
@@ -95,7 +95,7 @@ function saveDynamicDataToFile() {
 	let data = "Learning how to write in a file."
       
     // Write data in 'Output.txt' .
-    fs.writeFile('./uploadfiles/Output.txt', data, (err) => {
+    fs.writeFile('Output.txt', data, (err) => {
           
         // In case of a error throw err.
         if (err) throw err;
